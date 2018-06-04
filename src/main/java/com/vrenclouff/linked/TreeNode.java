d3324858ca818817b1104613f7c9e5a7e191924f
Lukas Cerny <luc.cerny@gmail.com>
2018-06-04 22:58:07 +0200

    Change access modifier

--- src/main/java/com/vrenclouff/linked/TreeNode.java
+++ src/main/java/com/vrenclouff/linked/TreeNode.java
@@ -5,35 +5,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-public class TreeNode<T extends Serializable> implements Serializable {
+class TreeNode<T extends Serializable> implements Serializable {
 
     private final T value;
     private final TreeNode<T> parent;
     private final List<TreeNode<T>> children;
 
-    protected TreeNode(T value, TreeNode<T> parent) {
+    TreeNode(T value, TreeNode<T> parent) {
         this.value = value;
         this.parent = parent;
         this.children = new LinkedList<>();
     }
 
-    public T value() {
+    T value() {
         return value;
     }
 
-    public TreeNode<T> parent() {
+    TreeNode<T> parent() {
         return parent;
     }
 
-    public List<TreeNode<T>> children() {
+    List<TreeNode<T>> children() {
         return children;
     }
 
-    public boolean isLeaf() {
+    boolean isLeaf() {
         return children().isEmpty();
     }
 
-    public boolean isRoot() {
+    boolean isRoot() {
         return parent == null;
     }
 }
